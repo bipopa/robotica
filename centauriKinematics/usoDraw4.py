84ff4bb7f488ca4e6ffd4f42fcf97bf43eccb92d
vagrant <vagrant@rosmachine>
2023-09-25 23:28:19 +0000

    centauri_visual, este permite visualizar el urdf de centauri

--- centauriKinematics/usoDraw4.py
+++ centauriKinematics/usoDraw4.py
@@ -32,11 +32,10 @@ alfa = np.array([np.pi/2, 0, np.pi/2, np.pi/2, np.pi/2, 0])
 drawrobot3d4(teta, d, a, alfa)
 
 
-"""""
+""""""""""
 q = [np.pi, 0, 0.3, 0]
 teta = np.array([q[0], 0, 0, q[3]])
 d = np.array([0.4, q[1], q[2], 0.2])
 a = np.array([0, -0.1, 0, 0])
 alfa = np.array([0, -np.pi/2, 0, 0])
-drawrobot3d4(teta, d, a, alfa)
-"""
+drawrobot3d4(teta, d, a, alfa)"""
---
+++ centauri_visual/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+import os
+from glob import glob
+from setuptools import setup
+from setuptools import find_packages
+
+package_name = 'centauri_visual'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'urdf'), glob('urdf/*')),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
+        (os.path.join('share', package_name, 'meshes'), glob('meshes/*')),
+        (os.path.join('share', package_name, 'rviz'), glob('rviz/*')),
+     
+    ],
+
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='vagrant',
+    maintainer_email='vagrant@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
