25eb714873b1c4c5ca6f9086f911fa12ab549d98
Alvaro Jose Santos <69470882+bipopa@users.noreply.github.com>
2023-10-04 15:23:31 -0500

    se modifico el ejemplo de drawrobot

--- Cinematica parcial 2 centauri/usoDraw4.py
+++ Cinematica parcial 2 centauri/usoDraw4.py
@@ -12,35 +12,19 @@ l3 = 0.1052
 
 q = [0, 0, 0, 0,0,0]
 
-teta = np.array([q[0], q[1], q[2], q[3],q[4],q[5]])
-d = np.array([l0, 0, 0, l2, 0,l3])
-a = np.array([0, l1, 0, 0, 0, 0])
-alfa = np.array([np.pi/2, 0, np.pi/2, np.pi/2, np.pi/2, 0])
-drawrobot3d4(teta, d, a, alfa)
+drawrobot3d4(q, l0, l1, l2,l3)
 
 
 q = [0, np.pi/2, 0, 0,np.pi/2,0]
 
-teta = np.array([q[0], q[1], q[2], q[3],q[4],q[5]])
-d = np.array([l0, 0, 0, l2, 0,l3])
-a = np.array([0, l1, 0, 0, 0, 0])
-alfa = np.array([np.pi/2, 0, np.pi/2, np.pi/2, np.pi/2, 0])
-drawrobot3d4(teta, d, a, alfa)
+drawrobot3d4(q, l0, l1, l2,l3)
 
 q = [0, np.pi/2, np.pi/2, 0,0,0]
 
-teta = np.array([q[0], q[1], q[2], q[3],q[4],q[5]])
-d = np.array([l0, 0, 0, l2, 0,l3])
-a = np.array([0, l1, 0, 0, 0, 0])
-alfa = np.array([np.pi/2, 0, np.pi/2, np.pi/2, np.pi/2, 0])
-drawrobot3d4(teta, d, a, alfa)
+drawrobot3d4(q, l0, l1, l2,l3)
 
 
-"""""
+
 q = [np.pi, 0, 0.3, 0]
-teta = np.array([q[0], 0, 0, q[3]])
-d = np.array([0.4, q[1], q[2], 0.2])
-a = np.array([0, -0.1, 0, 0])
-alfa = np.array([0, -np.pi/2, 0, 0])
-drawrobot3d4(teta, d, a, alfa)
-"""
+drawrobot3d4(q, l0, l1, l2,l3)
+
