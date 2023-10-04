a877d80e1a6f8f15b25d6ac7323d3a902111adc0
Alvaro Jose Santos <69470882+bipopa@users.noreply.github.com>
2023-10-04 15:18:55 -0500

    se agregaron funciones para cada uno de los marcos de referencia

--- Cinematica parcial 2 centauri/directkinematic.py
+++ Cinematica parcial 2 centauri/directkinematic.py
@@ -16,3 +16,40 @@ def cdirecta_centauri(q, l0,l1, l2, l3):
     Tf = T01.dot(T12).dot(T23).dot(T34).dot(T45).dot(T56)
     print(Tf)
     return Tf
+
+def T01(q, l0):
+    """ Retorna los sistemas de referencia de cada eslabón con respecto a la base
+    """
+    # Transformaciones homogéneas de DH
+    T01out = Tdh(l0, q[0], 0, np.pi/2)
+    return T01out
+def T12(q, l1):
+    """ Retorna los sistemas de referencia de cada eslabón con respecto a la base
+    """
+    # Transformaciones homogéneas de DH
+    T12out = Tdh(0, np.pi/2+q[1], l1, 0)
+    return T12out
+def T23(q):
+    """ Retorna los sistemas de referencia de cada eslabón con respecto a la base
+    """
+    # Transformaciones homogéneas de DH
+    T23out = Tdh(0, np.pi/2+q[2], 0, np.pi/2)
+    return T23out
+def T34(q, l2):
+    """ Retorna los sistemas de referencia de cada eslabón con respecto a la base
+    """
+    # Transformaciones homogéneas de DH
+    T34out = Tdh(l2, q[3], 0, -np.pi/2)
+    return T34out
+def T45(q):
+    """ Retorna los sistemas de referencia de cada eslabón con respecto a la base
+    """
+    # Transformaciones homogéneas de DH
+    T45out = Tdh(0, q[4], 0, np.pi/2)
+    return T45out
+def T56(q, l3):
+    """ Retorna los sistemas de referencia de cada eslabón con respecto a la base
+    """
+    # Transformaciones homogéneas de DH
+    T56out = Tdh(l3, q[5], 0, 0)
+    return T56out
