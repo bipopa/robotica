da57f1b647070b9172f37db51c54c5c9174ca7c0
Alvaro Jose Santos <69470882+bipopa@users.noreply.github.com>
2023-10-04 15:17:06 -0500

    Update planifica4.py

--- Cinematica parcial 2 centauri/planifica4.py
+++ Cinematica parcial 2 centauri/planifica4.py
@@ -2,9 +2,9 @@
 #COMPLETO
 #SIN ERRORES
 import numpy as np
-from inversekinematic import cinematica_inversa_centauri
+from inversekinematic import cinematica_inversa_centaurirad
 
-def planifica4(p1, p2, n, s, a, npuntos,teta,d,al,alfa):
+def planifica4(p1, p2, n, s, a, npuntos,l0,l1,l2,l3):
     # Cálculo del vector unitario
     u = np.array(p2) - np.array(p1)
     mu = np.sqrt(u[0]**2 + u[1]**2 + u[2]**2)
@@ -14,7 +14,7 @@ def planifica4(p1, p2, n, s, a, npuntos,teta,d,al,alfa):
     dl = mu / (npuntos + 1)
     
     # Inicialización de la matriz mat_q
-    mat_q = np.zeros((4, npuntos + 2))
+    mat_q = np.zeros((6, npuntos + 2))
     #print(mat_q)
     for i in range(npuntos + 2):
         # Cálculo de la posición cartesiana actual de la mano del manipulador
@@ -23,7 +23,7 @@ def planifica4(p1, p2, n, s, a, npuntos,teta,d,al,alfa):
         Tr = np.array([n, s, a, p]).T
         # Cálculo de las coordenadas articulares
        
-        q = cinematica_inversa_centauri(Tr,teta,d,al,alfa)
+        q = cinematica_inversa_centaurirad(Tr,l0,l1,l2,l3)
         mat_q[:, i] = q.flatten()
         #print(q)
         #input()
